eafc33bad5c892fe25c39b8c57628e181fa4c976
Andrei <marinin1337@gmail.com>
2023-03-02 01:15:57 +0400

    desipher appeared

--- task2.py
+++ task2.py
@@ -2,9 +2,9 @@ import codecs
 
 
 def count_character_index(path: str):
-    '''
+    """
     Функция, подсчитывающая индекс частот появления букв и записывающая данную информацию в текстовый файл.
-    '''
+    """
     letter_counter = {}
     absolute_character_counter = 0
     f = codecs.open(u'' + path, "r", "utf-8")
@@ -23,5 +23,23 @@ def count_character_index(path: str):
     f.close()
 
 
-count_character_index("original.txt")
+def desipher(path: str, key: dict):
+    """
+    Дешифратор
+    """
+    f = codecs.open(u'' + path, "r", "utf-8")
+    new_txt = ""
+    for i in f.read():
+        if i in key.keys():
+            new_txt = new_txt + key[i]
+        else:
+            new_txt = new_txt + i
+    f.close()
+    print(new_txt)
+
+
+desipher("original.txt", {"2": " ", "К": "О", "Ь": "И", "t": "Б", "Ы": "Р", "r": "Я", "Д": "C", ">": "С", "О": "Р",
+                          "<": "В", "Б": "М", "Я": "Н", "Й": "Д", "1": "В", " ": "К", "Ч": "П", "0": "З", "М": "Ы",
+                          "Х": "А", "А": "У", "a": "Ч", "Л": "Ж", "8": "Г", "c": "Х", "Е": "Ф", "3": "Й", ",": "Ю",
+                          "b": "Б", ".": "Ц", "9": "Ш", "Ф": "Щ", "?": "Э"})
 
